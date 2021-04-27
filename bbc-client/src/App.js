6de069b029c290b23d362c4490db1b3fdf30d59c
Ayelet Danieli <ayeletd@gmail.com>
2021-04-28 02:53:23 +0300

    add footer

--- bbc-client/src/App.js
+++ bbc-client/src/App.js
@@ -2,34 +2,38 @@ import React from "react";
 import SearchBar from "./Components/SearchComponent/SearchBar";
 import Activities from "./Components/SearchComponent/Activities";
 import Comments from "./Components//ArticleComponent/Comments";
+import Footer from "./Components/Footer/Footer";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div>
-      <div className="landingContainer">
-        <div className="wrapper">
-          <h1>BBC Science-Environment Translator</h1>
-          <SearchBar />
+      <div>
+        <div className="landingContainer">
+          <div className="wrapper">
+            <h1>BBC Science-Environment Translator</h1>
+            <SearchBar />
+          </div>
         </div>
-      </div>
-      <div className="landingContainer">
-        <h1>Latest:</h1>
+        <div className="landingContainer">
+          <h1>Latest:</h1>
 
-        <Activities />
-      </div>
+          <Activities />
+        </div>
 
-      <Comments
-        comment={"this is so good!!!! love it!! "}
-        author={"Orel Alon"}
-        time={"2 days ago"}
-      />
-      <Comments
-        comment={"Great article, I would love to see more"}
-        author={"Itai Levi"}
-        time={"20 minutos ago"}
-      />
+        <Comments
+          comment={"this is so good!!!! love it!! "}
+          author={"Orel Alon"}
+          time={"2 days ago"}
+        />
+        <Comments
+          comment={"Great article, I would love to see more"}
+          author={"Itai Levi"}
+          time={"20 minutos ago"}
+        />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ bbc-client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h4>The BBC Project was served to you by</h4>
+
+      <div class="names">
+        <ul>
+          <a href="https://www.linkedin.com/in/orel-alon-090689184/">
+            Orel Alon
+          </a>
+
+          <a href="https://www.linkedin.com/in/ayeletdanieli/">
+            Ayelet Danieli
+          </a>
+
+          <a href="https://www.linkedin.com/in/adanhy/">Adan Hajyahia</a>
+
+          <a href="https://www.linkedin.com/in/guy-shefer-5330791b3/">
+            Guy Shefer
+          </a>
+
+          <a href="https://www.linkedin.com/in/david-sasson-67229219b/">
+            Daviv Sasson
+          </a>
+
+          <a href="https://www.linkedin.com/in/gurelbs/">Gurel Ben-Shabat</a>
+
+          <a href="https://www.linkedin.com/in/alex-dubov-3ba25332/">
+            Alex Dubov
+          </a>
+
+          <a href="https://www.linkedin.com/in/roy-dotan-436598103/">
+            Roy Dotan
+          </a>
+        </ul>
+      </div>
+    </footer>
+  );
+}
