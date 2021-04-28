952626da08debd5371274d232be5bd03c5370132
Adan Hajyahya <the.adan.hy@gmail.com>
2021-04-28 11:03:04 +0300

    bbc proj

--- bbc-client/src/Pages/About/About.page.jsx
+++ bbc-client/src/Pages/About/About.page.jsx
@@ -1,85 +1,85 @@
 import "./AboutStyle.css";
-
 const AboutPage = () => {
   return (
     <>
-      <h2>Contributers:</h2>
-      <div class="grid-container">
-        <div class="Ayelet">
-          Ayelet Danieli
-          <a href="https://www.linkedin.com/in/ayeletdanieli/">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/ayelet">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <div class="Gurel">
-          Gurel Ben-Shabat
-          <a href="https://www.linkedin.com/in/gurelbs/">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/gurelbs">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <div class="Orel">
-          Orel Alon
-          <a href="https://www.linkedin.com/in/orel-alon-090689184/">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/OrelAlon">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <div class="Roy">
-          Roy Dotan
-          <a href="https://www.linkedin.com/in/roy-dotan-436598103/">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="/">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <div class="Guy">
-          Guy Shefer
-          <a href="https://www.linkedin.com/in/guy-shefer-5330791b3/">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/GuyShefer">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <div class="David">
-          Daviv Sasson
-          <a href="https://www.linkedin.com/in/david-sasson-67229219b/">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/DavidSasson22">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <div class="Alex">
-          Alex Dubov
-          <a href="https://www.linkedin.com/in/alex-dubov-3ba25332/">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="/">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <div class="Adan">
-          Adan Hajyahia
-          <a href="https://www.linkedin.com/in/adanhy/">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/adanhy">
-            <i class="fab fa-github"></i>
-          </a>
+      <div className="about">
+        <h2>Our Team</h2>
+        <div className="grid-container">
+          <div className="Ayelet name">
+            Ayelet Danieli
+            <a href="https://www.linkedin.com/in/ayeletdanieli/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/ayelet">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+          <div className="Gurel name">
+            Gurel Ben-Shabat
+            <a href="https://www.linkedin.com/in/gurelbs/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/gurelbs">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+          <div className="Orel name">
+            Orel Alon
+            <a href="https://www.linkedin.com/in/orel-alon-090689184/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/OrelAlon">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+          <div className="Roy name">
+            Roy Dotan
+            <a href="https://www.linkedin.com/in/roy-dotan-436598103/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="/">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+          <div className="Guy name">
+            Guy Shefer
+            <a href="https://www.linkedin.com/in/guy-shefer-5330791b3/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/GuyShefer">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+          <div className="David name">
+            Daviv Sasson
+            <a href="https://www.linkedin.com/in/david-sasson-67229219b/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/DavidSasson22">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+          <div className="Alex name">
+            Alex Dubov
+            <a href="https://www.linkedin.com/in/alex-dubov-3ba25332/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="/">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+          <div className="Adan name">
+            Adan Hajyahia
+            <a href="https://www.linkedin.com/in/adanhy/">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/adanhy">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
         </div>
       </div>
     </>
   );
 };
-
 export default AboutPage;
