e8beb4a72d95f19e05707ae915917305337954f2
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-04-28 03:22:06 +0300

    update

--- bbc-client/src/App.js
+++ bbc-client/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import SearchBar from "./Components/SearchComponent/SearchBar";
 import Activities from "./Components/SearchComponent/Activities";
 import Comments from "./Components//ArticleComponent/Comments";
 import Footer from "./Components/Footer/Footer";
 
->>>>>>> 3c254f40dad94e86db35071d0c4a135194dbd4b1
 import "./App.css";
 import ArticlePage from "./Components/ArticlePage/ArticlePage.component";
 import Navbar from "./Components/Navbar/Navbar.componenet";
@@ -15,11 +12,7 @@ import Main from "./Components/Main/Main.components";
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <div className='homepage'>
-=======
-    <div>
->>>>>>> 3c254f40dad94e86db35071d0c4a135194dbd4b1
       <Router>
         <Navbar />
         <Switch>
@@ -53,6 +46,3 @@ export default function App() {
     </div>
   );
 }
-
-
-
