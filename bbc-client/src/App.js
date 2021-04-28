f000af76a7d3f94c4265ebd8377015c4036a7f5c
gurelbs <64422983+gurelbs@users.noreply.github.com>
2021-04-28 03:19:29 +0300

    update

--- bbc-client/src/App.js
+++ bbc-client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import Comments from "./Components//ArticleComponent/Comments";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import "./App.css";
 import ArticlePage from "./Components/ArticlePage/ArticlePage.component";
 import Navbar from "./Components/Navbar/Navbar.componenet";
@@ -9,18 +7,7 @@ import Main from "./Components/Main/Main.components";
 
 export default function App() {
   return (
-    <div>
-      {/* <Comments
-        comment={"this is so good!!!! love it!! "}
-        author={"Orel Alon"}
-        time={"2 days ago"}
-      />
-      <Comments
-        comment={"Great article, I would love to see more"}
-        author={"Itai Levi"}
-        time={"20 minutos ago"}
-      /> */}
-
+    <div className='homepage'>
       <Router>
         <Navbar />
         <Switch>
@@ -29,8 +16,6 @@ export default function App() {
         </Switch>
       </Router>
     </div>
-
-
   );
 }
 
