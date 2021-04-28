579876270d849f04cfc9a46ddd83ab3257ebfebf
Adan Hajyahya <the.adan.hy@gmail.com>
2021-04-28 03:34:37 +0300

    team 5 03:34 am 28.4.21

--- bbc-client/src/Pages/ArticlePage/ArticlePage.component.jsx
+++ bbc-client/src/Pages/ArticlePage/ArticlePage.component.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import Comment from "../../Components/Comment/Comment.component";
 
+const comments = [
+  { time: new Date(), author: "Ronaldo", comment: "siiiiiiiiiiiiiiiiii" },
+  {
+    time: new Date(),
+    author: "Ronaldo",
+    comment: "siiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+  },
+  { time: new Date(), author: "Ronaldo", comment: "siiiiiiiiiiiiiiiiii" },
+  {
+    time: new Date(),
+    author: "Ronaldo",
+    comment: "siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+  },
+];
+
 const ArticlePage = (props) => {
+  const renderComents = () => {
+    return comments.map((v) => {
+      //
+      return <Comment time={v.time} author={v.author} comment={v.comment} />;
+      //
+    });
+  };
+
   return (
     <div>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      {props.comments.map((v) => {
-        //
-        <Comment time={v.time} author={v.author} comment={v.comment} />;
-        //
-      })}
-      <Comment
-        comment={"this is so good!!!! love it!! "}
-        author={"Orel Alon"}
-        time={"2 days ago"}
-      />
-      <Comment
-        comment={"Great article, I would love to see more"}
-        author={"Itai Levi"}
-        time={"20 minutos ago"}
-      />
+      {renderComents()}
     </div>
   );
 };
